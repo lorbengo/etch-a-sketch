b4ab50d30f84356217a9e3fdf23f38c84c50aded
lorbengo <lorbengo@gmail.com>
2024-02-17 13:26:29 +0300

    Set up a hover effect

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const grid = document.getElementById("grid");
 const fragment = new DocumentFragment();
 
-for (let row = 0; row < 16; row++) {
-  for (let col = 0; col < 16; col++) {
+for (let row = 1; row <= 16; row++) {
+  for (let col = 1; col <= 16; col++) {
     const cell = document.createElement("div");
-    cell.style.cssText =
-      "width: 16px; height: 16px; border-right: 1px solid black; border-bottom: 1px solid black";
+    cell.style.cssText = "width: 16px; height: 16px";
     fragment.appendChild(cell);
   }
 }
-
 grid.appendChild(fragment);
+
+grid.addEventListener("mouseover", changeBackgroundColorCells);
+grid.addEventListener("mousedown", changeBackgroundColorCell);
+
+function changeBackgroundColorCells(event) {
+  if (event.altKey) {
+    event.target.style.backgroundColor = "black";
+  }
+}
+
+function changeBackgroundColorCell(event) {
+  event.target.style.backgroundColor = "black";
+}
