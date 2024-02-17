002b9df27c3df5b4e1b1708f3346bf23b10e8f0b
lorbengo <lorbengo@gmail.com>
2024-02-17 15:51:55 +0300

    Add a popup for generate new grid

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const grid = document.getElementById("grid");
 const fragment = new DocumentFragment();
+const promptButton = document.getElementById("prompt");
 
-for (let row = 1; row <= 16; row++) {
-  for (let col = 1; col <= 16; col++) {
-    const cell = document.createElement("div");
-    cell.style.cssText = "width: 16px; height: 16px";
-    fragment.appendChild(cell);
-  }
-}
-grid.appendChild(fragment);
-
+window.addEventListener("DOMContentLoaded", () => gridGenerator());
 grid.addEventListener("mouseover", changeBackgroundColorCells);
 grid.addEventListener("mousedown", changeBackgroundColorCell);
+promptButton.addEventListener("click", gridRegenerator);
+
+function gridGenerator(numberSquares = 16) {
+  const size = 640 / numberSquares;
+  for (let row = 1; row <= numberSquares; row++) {
+    for (let col = 1; col <= numberSquares; col++) {
+      const cell = document.createElement("div");
+      cell.style.cssText = `width: ${size}px; height: ${size}px`;
+      fragment.appendChild(cell);
+    }
+  }
+  grid.appendChild(fragment);
+}
 
 function changeBackgroundColorCells(event) {
   if (event.altKey) {
@@ -21,4 +27,16 @@ function changeBackgroundColorCells(event) {
 
 function changeBackgroundColorCell(event) {
   event.target.style.backgroundColor = "black";
+}
+
+function gridRegenerator() {
+  let numberSquares = prompt("Please enter the number of squares");
+  if (!numberSquares) {
+    numberSquares = 16;
+  }
+  if (numberSquares > 100) {
+    numberSquares = 100;
+  }
+  grid.innerHTML = "";
+  gridGenerator(numberSquares);
 }
